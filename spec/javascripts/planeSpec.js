8186ea7a074298498860e3a695312b354b7845c1
krisswiltshire30 <kriss.wiltshire@googlemail.com>
2019-08-20 14:14:05 +0100

    Red - Feature test - Planes can be instructed to land at an airport

---
+++ spec/javascripts/planeSpec.js
@@ -0,0 +1,21 @@
+// describe("Plane", function() {
+//   var plane1;
+//   var plane2;
+//
+//   beforeEach(function() {
+//     plane1 = new Plane();
+//     plane2 = new Plane();
+//   });
+//
+//   it("asd", function() {});
+//
+//   describe("test", function() {
+//     it("can create a new plane", function() {
+//       expect(plane1).toEqual(plane2);
+//     });
+//   });
+//
+//   describe("this is a test",function() {
+//     it("can do a thing", )
+//   });
+// });
